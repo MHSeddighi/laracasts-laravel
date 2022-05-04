4fab3b8f8a907ae7d2ac5f2162b12c1e1b5ee011
MHSeddighi <93768431+MHSeddighi@users.noreply.github.com>
2022-05-04 12:16:06 +0430

    update:profile page(add hover effect on profile menu)

--- laracast/resources/views/layouts/profile.blade.php
+++ laracast/resources/views/layouts/profile.blade.php
@@ -8,60 +8,100 @@
         position: fixed;
         top: 0;
         right: 0;
-        width: 45%;
+        width: 50%;
         height: 100%;
         display: flex;
+        flex-direction: column;
+        pointer-events: auto;
         background-color: #0D131D;
-        background-clip: padding-box;
         border: 1px solid rgba(0, 0, 0, 0.2);
         outline: 0;
     }
-    .dec-none a{
-        text-decoration:none;
+
+    .dec-none a {
+        text-decoration: none;
+    }
+
+    .profile-menu {
+        padding-bottom: 100px;
+        border-radius: 10px;
+        font-size: 15px;
+        letter-spacing: 1.2px;
+    }
+
+    .profile-menu a {
+        text-decoration: none;
+        padding-left: 10px;
+        padding-right: 80px;
+    }
+
+    .profile-menu a::before {
+        background: linear-gradient(180deg, #151c26 45%, transparent 0, transparent 55%, #151c26 0);
+        border-radius: 4px;
+        content: "";
+        display: block;
+        height: 100%;
+        position: absolute;
+        left: -5px;
+        width: 4px;
+    }
+
+    .profile-menu a:hover:before {
+        background: linear-gradient(180deg, #1156c5 45%, transparent 0, transparent 55%, #1156c5 0);
+    }
+
+    .profile-menu a > *{
+        color:white;
+    }
+    .profile-menu a:hover > *{
+        color: blue;
     }
 </style>
 
 <div class="modal-dialog">
-    <div class="py-5 px-5 text-white modal-profile gap-5">
-        <div class="d-flex flex-column gap-3 f-bold dec-none code-link">
-            <a class="my-1" href="#">
-                <div class="fs-5 text-white">What's New</div>
-                <span class="font-size-10 font-gray">//new content for you</span>
-            </a>
+    <div class="py-5 px-4 modal-profile gap-4">
+        <div class="text-center fs-5 text-white">Notifications</div>
+        <div class="d-flex">
+            <div class="d-flex flex-column gap-2 dec-none profile-menu ">
+                <a class="my-1" href="#">
+                    <div class="fs-5 ">What's New</div>
+                    <span class="font-size-10 font-gray">//new content for you</span>
+                </a>
 
-            <a class="my-1" href="#">
-                <div class="fs-5 text-white">My Library</div>
-                <span class="font-size-10 font-gray">//keep going</span>
-            </a>
+                <a class="my-1" href="#">
+                    <div class="fs-5 ">My Library</div>
+                    <span class="font-size-10 font-gray">//keep going</span>
+                </a>
 
-            <a class="my-1" href="#">
-                <div class="fs-5 text-white">Social Feed</div>
-                <span class="font-size-10 font-gray">//new from the community</span>
-            </a>
+                <a class="my-1" href="#">
+                    <div class="fs-5 ">Social Feed</div>
+                    <span class="font-size-10 font-gray">//new from the community</span>
+                </a>
 
-            <a class="my-1" href="#">
-                <div class="fs-5 text-white">My Profile</div>
-                <span class="font-size-10 font-gray">//everyone sees this page</span>
-            </a>
+                <a class="my-1" href="#">
+                    <div class="fs-5 ">My Profile</div>
+                    <span class="font-size-10 font-gray">//everyone sees this page</span>
+                </a>
 
-            <a class="my-1" href="#">
-                <div class="fs-5 text-white">Settings</div>
-                <span class="font-size-10 font-gray">//make a twaek</span>
-            </a>
+                <a class="my-1" href="#">
+                    <div class="fs-5 ">Settings</div>
+                    <span class="font-size-10 font-gray">//make a twaek</span>
+                </a>
 
-            <a class="my-1" href="#">
-                <div class="fs-5 text-white">Gift an Acount</div>
-                <span class="font-size-10 font-gray">from the kidness of yout <'3 </span>
-            </a>
+                <a class="my-1" href="#">
+                    <div class="fs-5 ">Gift an Acount</div>
+                    <span class="font-size-10 font-gray">//from the kidness of yout <3 </span>
+                </a>
 
-            <a class="my-1" href="#">
-                <div class="fs-5 text-white">Logout</div>
-                <span class="font-size-10 font-gray">//but...why?</span>
-            </a>
-        </div>
-        <div class="line"></div>
-        <div>
+                <a class="my-1" href="#">
+                    <div class="fs-5 ">Logout</div>
+                    <span class="font-size-10 font-gray">//but...why?</span>
+                </a>
+            </div>
+            <div class="line"></div>
+            <div>
 
+            </div>
         </div>
     </div>
 </div>
