24d5ef96e3a13c9e9da4b7478bc8aefa43e00c07
MHSeddighi <93768431+MHSeddighi@users.noreply.github.com>
2022-07-19 19:44:49 +0430

    update login and register forms

--- laracast/resources/views/auth/login.blade.php
+++ laracast/resources/views/auth/login.blade.php
@@ -1,19 +1,9 @@
-<div class="modal-dialog login-form">
-    <form class="p-4 pt-5" action="{{ route('login.store') }}" method="Post" style="position: fixed;
-        left:50%;
-        transform:translateX(-50%);
-        border-radius:15px;
-        width:25%;
-        height:80%;
-        display: flex;
-        flex-direction: column;
-        pointer-events: auto;
-        background:#fff;
-        border: 1px solid rgba(0, 0, 0, 0.2);
-        outline: 0;">
-
+<div class="modal-dialog ">
+    <form class="p-4 pt-5 login-form" action="{{ route('login.store') }}" method="Post">
         @csrf
-
+        <span class="fs-2 fw-bolder text-center" style="font-family:cabinet;">Welcome to the 
+            Laracast
+        </span>
         @if($errors->any())
         <div class="alert alert-danger">
             @foreach($errors->all() as $error)
@@ -21,26 +11,27 @@
             @endforeach
         </div>
         @endif
-        <input class="login-input bg-light border-1 border-radius-1 px-3" id="username" name="username" type="text" placeholder="{{__('Username')}}" autofocus><br><br>
+        <input class="login-input bg-light border-1 border-radius-1 px-3" id="username" name="username" type="text" placeholder="{{__('Username')}}" autofocus>
 
-        <input class="login-input bg-light border-1 border-radius-1 px-3 mb-1" name="password" id="password" type="password" placeholder="{{__('Password')}}"><br>
+        <input class="login-input bg-light border-1 border-radius-1 px-3 mb-1" name="password" id="password" type="password" placeholder="{{__('Password')}}">
 
         <div>
             <input class="form-check-input" type="checkbox" id="remember-me" name="remember">
-            <label class="form-check-label underline-hover fs-6" for="remember-me">{{__('Remember me')}}</label><br><br>
+            <label class="form-check-label underline-hover fs-6" for="remember-me">{{__('Remember me')}}</label>
         </div>
-        <button class="btn submit btn-primary fs-6 w-100" id="submit">
+        <button class="btn submit btn-primary fs-6 w-100 login-button" id="submit">
             {{ __('SIGN IN')}}
         </button>
-        <hr size="3" style="width:100%">
 
-        <button class="ps-3 mb-3 btn btn-outline-warning">
-            <img  class="me-1 my-auto" src="images/google.svg" width="15px" height="15px">
+        <div class="line">
+            <span>or</span>
+        </div>
+
+        <button class="btn btn-outline-warning login-google-button">
             <span>{{ __('Continue with Google')}}</span>
         </button>
 
-        <button class="ps-3 btn btn-outline-primary">
-            <img  class="me-1 my-auto" src="images/facebook.svg" width="15px" height="15px">
+        <button class="btn btn-outline-primary login-facebook-button">
             <span>{{ __('Continue with Facebook')}}</span>
         </button>
     </form>
--- laracast/resources/views/auth/register.blade.php
+++ laracast/resources/views/auth/register.blade.php
@@ -1,38 +1,33 @@
-<div class="container mx-auto my-5 col-md-8" >
-    <div class="card p-4 bg-light border-radius-2 shadow-sm" style="filter:opacity(80%) blure(5px);">
+<div class="modal-dialog">
+    <div class="card p-4 bg-light border-radius-2 register-form">
+        <form action="{{ route('register') }}" method="Post">
+            <div class="text-center fs-3 mb-3" style="font-family:cabinet;">
+                {{__('Create account')}}
+            </div>
+            @csrf
 
-        <div class="card-header ps-5 fs-4">
-            {{__('Register')}}
-        </div>
-        <div class="card-body fs-5 py-5">
-            <form action="{{ route('register') }}" method="Post">
-                @csrf
+            @if($errors->any())
+            <div class="alert alert-danger">
+                @foreach($errors->all() as $error)
+                <div>{{ $error }}</div>
+                @endforeach
+            </div>
+            @endif
+            <label class="col-md-4 " for="username">{{__('Username')}} :</label>
+            <input class="bg-light border-1 border-radius-2 register-input" name="username" id="username" type="text" placeholder="username or email" value="{{ old('username') }}" autofocus><br><br>
 
-                @if($errors->any())
-                    <div class="alert alert-danger">
-                        @foreach($errors->all() as $error)
-                            <div>{{ $error }}</div>
-                        @endforeach
-                    </div>
-                @endif
-                <label class="col-md-4 " for="username">{{__('Username')}}</label>
-                <input class="bg-light border-1 border-radius-2 px-3 py-1" name="username" id="username" 
-                type="text" placeholder="username or email" value="{{ old('username') }}" autofocus><br><br>
+            <label class="col-md-4 form-label" for="email">{{__('Email')}} :</label>
+            <input class="bg-light border-1 border-radius-2 register-input" name="email" id="email" type="text" value="{{ old('email') }}"><br><br>
 
-                <label class="col-md-4 form-label" for="email">{{__('Email')}}</label>
-                <input class="bg-light border-1 border-radius-2 px-3 py-1" name="email" id="email" type="text" value="{{ old('email') }}"><br><br>
+            <label class="col-md-4 form-label" for="password">{{__('Password')}} :</label>
+            <input class="bg-light border-1 border-radius-2 register-input" name="password" id="password" type="password" value="{{ old('password') }}"><br><br>
 
-                <label class="col-md-4 form-label" for="password">{{__('Password')}}</label>
-                <input class="bg-light border-1 border-radius-2 px-3 py-1" name="password" id="password" type="password" value="{{ old('password') }}"><br><br>
+            <label class="col-md-4 form-label" for="confirm-password">{{__('Confirm password')}} :</label>
+            <input name="password_confirmation" class="bg-light border-1 border-radius-2 register-input" id="confirm-password" type="password"><br><br>
 
-                <label class="col-md-4 form-label" for="confirm-password">{{__('Confirm password')}}</label>
-                <input name="password_confirmation" class="bg-light border-1 border-radius-2 px-3 py-1" id="confirm-password" type="password"><br><br>
-
-                <hr>
-                <button class="btn submit btn-primary fs-5 align-item-center" id="submit">
-                    {{ __('Register')}}
-                </button>
-            </form>
-        </div>
+            <button class="btn submit btn-primary fs-5 align-item-center w-100" id="submit">
+                {{ __('Sign Up')}}
+            </button>
+        </form>
     </div>
 </div>
